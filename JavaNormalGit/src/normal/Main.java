8cd76a660796349a0b25750e7fb52368fbb34db8
camposteladm <camposteladm@gmail.com>
2015-03-18 13:50:58 -0500

    git1

---
+++ JavaNormalGit/src/normal/Main.java
@@ -0,0 +1,10 @@
+package normal;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("JAVA NORMAL GIT");
+
+	}
+
+}
---
+++ JavaNormalGit/src/normal/NormalTest.java
@@ -0,0 +1,14 @@
+package normal;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NormalTest {
+
+	@Test
+	public void test() {
+		assertTrue(true); 
+	}
+
+}
